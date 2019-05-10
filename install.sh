0a8af073fbabf01973c38f9b5eead471d4d65b53
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-05-10 23:05:04 +0530

    After installation setup

---
+++ install.sh
@@ -0,0 +1,4 @@
+cp /opt/QRScanner/QRScanner.png ./sun-java.png
+cp /opt/QRScanner/QRScanner.png ./sun-java_HighContrast.png
+cp /opt/QRScanner/QRScanner.png ./sun-java_HighContrastInverse.png
+cp /opt/QRScanner/QRScanner.png ./sun-java_LowContrast.png
